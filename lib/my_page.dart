d6ac428ac08ee4e710dbba49b710c332e793eccc
asmaakhaled1 <asmaakhaled1411@gmail.com>
2025-09-06 18:26:13 +0300

    Update pubspec.yaml and add new pages

---
+++ lib/my_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:guidey/assistant_screen.dart';
+import 'package:guidey/home_screen.dart';
+import 'package:guidey/jobs_screen.dart';
+import 'package:guidey/quiz/quiz_page.dart';
+import 'career_search_page.dart';
+
+
+class MyPage extends StatefulWidget {
+  const MyPage({super.key});
+
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> pages = [
+    const HomeScreen(),
+    QuizScreen(),
+    CareerSearchPage(),
+    AssistantScreen(),
+    JobsScreen(),
+  ];
+
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.blue[900]!,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.brain),
+            label: 'Quiz',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.map),
+            label: 'Roadmap',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble_outline),
+            label: 'Assistant',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_repair_service_rounded),
+            label: 'Jobs',
+          ),
+        ],
+      ),
+    );
+  }
+}
